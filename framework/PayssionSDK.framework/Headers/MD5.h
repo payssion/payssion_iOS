07760773f20bd8b250fc4a84f60016c76ec4f4da
Schiffe <ulquiorra.cifer@live.cn>
2017-06-19 11:12:27 +0800

    update change

---
+++ framework/PayssionSDK.framework/Headers/MD5.h
@@ -0,0 +1,15 @@
+//
+//  MD5.h
+//  Payssion-Example
+//
+//  Created by UlquiorraCifer on 16/11/22.
+//  Copyright © 2016年 Payssion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MD5 : NSObject
+
++(NSString *) md5: (NSString *) inPutText;
+
+@end
